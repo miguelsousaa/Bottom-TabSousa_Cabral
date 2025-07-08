0e79e103d107f4129ea53c6501818f26da20b335
Miguel Sousa <161040675+miguelsousaa@users.noreply.github.com>
2025-07-07 21:23:07 -0300

    Sousa Cabral

--- App.tsx
+++ App.tsx
@@ -4,7 +4,6 @@ import AppRoutes from './src/routes'; // Importando nossas rotas
 
 export default function App() {
   return (
-    // O NavigationContainer é o componente raiz que gerencia a navegação
     <NavigationContainer>
       <AppRoutes />
     </NavigationContainer>
