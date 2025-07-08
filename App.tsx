250d146be27e39bd6feaa4d58da56ff4ce457fe7
Miguel Sousa <161040675+miguelsousaa@users.noreply.github.com>
2025-07-07 21:24:26 -0300

    Sousa Cabral

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import AppRoutes from './src/routes'; // Importando nossas rotas
+import AppRoutes from './src/routes'; 
 
 export default function App() {
   return (
